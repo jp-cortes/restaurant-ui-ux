d1aef658b262f7caf5220df076b320ccb5940844
Juan Cortes <alias99002@outlook.com>
2023-04-01 13:39:44 -0500

    section about compled

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
-
+import { images } from '../../constants';
 import './AboutUs.css';
+
 
 export function AboutUs() {
   return (
-    <div>
-      AboutUs
+    <div className='app__aboutus app__bg flex__center section__padding' id='about'>
+      <div className='app__aboutus-overlay flex__center'>
+        <img src={images.G} alt='G_overlay'/>
+      </div>      
+
+      <div className='app__aboutus-content flex__center'>
+        <div className='app__aboutus-content_about'>
+          <h2 className='headtext__cormorant'>About Us</h2>
+          <img src={images.spoon} alt="about__spoon" className='spoon__img'/>
+          <p className='p__opensans'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error corporis omnis nobis at cupiditate ad eligendi reiciendis ut provident molestiae? Laboriosam eos atque sed fugiat cupiditate iure blanditiis totam dolor?</p>
+          <button type='button' className='custom__button'>Know More</button>
+        
+         </div>
+
+          <div className='app__aboutus-content_knife flex__center'>
+          <img src={images.knife} alt="about_knife"/>
+          </div>
+
+
+         <div className='app__aboutus-content_history '>
+        
+          <h2 className='headtext__cormorant'>Our History</h2>
+          <img src={images.spoon} alt="about__spoon" className='spoon__img'/>
+          <p className='p__opensans'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error corporis omnis nobis at cupiditate ad eligendi reiciendis ut provident molestiae? Laboriosam eos atque sed fugiat cupiditate iure blanditiis totam dolor?</p>
+          <button type='button' className='custom__button'>Know More</button>
+        </div>
+        
+      </div>
+
     </div>
   );
 }
