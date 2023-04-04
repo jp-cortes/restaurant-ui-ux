3ab323f15193cd38951a3192997d06981e35e518
Juan Cortes <alias99002@outlook.com>
2023-04-04 15:24:52 -0500

    intro, laurels section done

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
-
+import React, { useRef, useState } from 'react';
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
+import { meal } from '../../constants';
 import './Intro.css';
 
 export function Intro() {
+  const [playVideo, setPlayVideo] = useState(false);
+  const videoRef = useRef();
+
+  function handleVideo() {
+    setPlayVideo((prevState) => !prevState);
+
+    if(playVideo){
+      videoRef.current.pause();
+    } else {
+      videoRef.current.play();
+
+    }
+  }
+
   return (
-    <div>
-      Intro
+    <div className='app__video'>
+      <video
+      ref={videoRef}
+      src={meal}
+      type='video/mp4'
+      loop
+      controls={false}
+      muted
+      /> 
+      <div className='app__video-overlay flex__center'>
+        <div
+        className='app__video-overlay_circle flex__center'
+        onClick={handleVideo}
+        >
+          {
+          playVideo ? (<BsPauseFill color='#fff' fontSize={30}/>) : 
+          (<BsFillPlayFill color='#fff' fontSize={30}/>)
+          }
+
+        </div>
+        
+        </div>     
     </div>
   );
 }
--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import { SubHeading } from '../../components';
+import { images, data } from '../../constants';
 
 import './Laurels.css';
 
+function AwardCard({award: { imgUrl, title, subtitle }}) {
+  return (
+    <div className='app__laurels_awards-card'>
+      <img src={imgUrl} alt='awards'/>
+      <div className='app__laurels_awards-card_content'>
+        <p className='p__cormorant' style={{ color: '#DCCA87'}}>{title}</p>
+        <p className='p__opensans'>{subtitle}</p>
+      </div>
+    </div>
+  );
+}
+
 export function Laurels() {
   return (
-    <div>
-      Laurels
+    <div className='app__bg app__wrapper section__padding' id='awards'>
+      <div className='app__wrapper_info'>
+          <SubHeading title="Awards & Recognition"/>
+          <h2 className='headtext__cormorant'>Our Laurels</h2>
+
+          <div className='app__laurels_awards'>
+            {data.awards.map((award) => <AwardCard award={award} key={award.title}/>)}
+          </div>        
+        </div>  
+        <div className='app__wrapper_img'>
+          <img src={images.laurels} alt='laurels_image'/>
+          </div>    
     </div>
   );
 }
