d55796caaa61f5c66df6f9243e2c2608f2f89ae2
Juan Cortes <alias99002@outlook.com>
2023-04-05 14:06:38 -0500

    findus & gallery containers done

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 
+import { images } from '../../constants';
+import { SubHeading } from '../../components';
+
 export function FindUs() {
   return (
-    <div>
-      FindUs
+    <div className='app__bg app__wrapper section__padding' id='contact'>
+      <div className='app__wrapper_info'>
+        <SubHeading title='Contact'/>
+        <h2 className='headtext__cormorant' style={{ marginBottom: '3rem'}}>Find Us</h2>
+        <div className='app__wrapper-content'>
+          <p className='p__opensans'>Lane Ends Bungalow, Whatcroft Hall Lane, Rudheath, CW9 75G</p>
+          <p className='p__cormorant' style={{ color: '#DCCA87', margin: '2rem 0'}}>Opening Hours</p>
+          <p className='p__opensans'>Mon - Fri: 10:00am - 02:00am</p>
+          <p className='p__opensans'>Sat - Sun: 10:00am - 03:00am</p>
+        </div>
+          <button className='custom__button' style={{ marginTop: '2rem'}}>Visit Us</button>
+      </div>
+
+      <div className='app__wrapper_img'>
+        <img src={images.findus} alt='find-us_image'/>
+      </div>
+      
     </div>
   );
 }
--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
 
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 import './Gallery.css';
 
 export function Gallery() {
+const scrollRef = useRef();
+
+function scroll(direction) {
+  const { current } = scrollRef;
+
+  if(direction === 'left') {
+    current.scrollLeft -= 300;
+  } else {
+    current.scrollLeft += 300;
+  }
+}
+
   return (
-    <div>
-      Gallery
+    <div className='app__gallery flex__center'>
+      <div className='app__gallery-content'>
+        <SubHeading title='Instagram'/>
+        <h2 className='headtext__cormorant'>Photo Gallery</h2>
+        <p className='p__opensans' style={{ color: '#AAAAAA', marginTop: '2rem' }}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto quam temporibus placeat repudiandae, atque sequi dignissimos asperiores totam facere recusandae dolorum. Sint ducimus laborum nostrum eligendi libero quod similique beatae.</p>
+        <button className='custom__button'>View More</button>
+      </div>
+      <div className='app__gallery-images'>
+        <div className='app__gallery-images-container' ref={scrollRef}>
+          {[images.gallery01, images.gallery02, images.gallery03, images.gallery04].map((image, index) => (
+            <div className='app__gallery-images_card flex__center' key={`gallery_image-${index + 1}`}>
+              <img src={image} alt='gallery_image'/>
+              <BsInstagram className='gallery__image-icon'/>
+            </div>
+          ))}
+        </div>
+        <div className='app__gallery-images_arrows'>
+            <BsArrowLeftShort className='gallery__arrow-icon' onClick={() => scroll('left')}/>
+            <BsArrowRightShort className='gallery__arrow-icon' onClick={() => scroll('right')}/>
+        </div>
+      </div>
+      
     </div>
   );
 }
